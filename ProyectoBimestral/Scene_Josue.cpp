9719df2e74b490dd2568b3a6d6dec8a8c73e2176
Tu Nombre <josd.1523@gmail.com>
2026-07-13 06:37:40 -0500

    Implementacion de modelo 2 y entidad.

--- ProyectoBimestral/Scene_Josue.cpp
+++ ProyectoBimestral/Scene_Josue.cpp
@@ -1,7 +1,6 @@
 // ==============================
 //    ESCENA VR ROOM - Josue
 // ==============================
-
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
@@ -14,21 +13,29 @@
 #include <learnopengl/model.h>
 
 #include <iostream>
+#include <string>
 
 #define STB_IMAGE_IMPLEMENTATION
 #include <learnopengl/stb_image.h>
 #include <filesystem>
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
 void processInput(GLFWwindow* window);
+void ClampPlayerToRoom();
+
+bool pKeyPressedLastFrame = false;
 
 // settings
 const unsigned int SCR_WIDTH = 1280;
 const unsigned int SCR_HEIGHT = 720;
 
-// camera
-Camera camera(glm::vec3(0.0f, 1.0f, 5.0f));
+// =========================================================
+// ESCALA Y POSICIÓN
+// =========================================================
+float worldScale = 0.05f;
+Camera camera(glm::vec3(0.0f, 1.2f, 1.0f));
 float lastX = SCR_WIDTH / 2.0f;
 float lastY = SCR_HEIGHT / 2.0f;
 bool firstMouse = true;
@@ -36,6 +43,38 @@ bool firstMouse = true;
 // timing
 float deltaTime = 0.0f;
 float lastFrame = 0.0f;
+float gameStartTime = 0.0f; // NUEVO: Temporizador para controlar el inicio del nivel
+
+// --- Entidad que persigue al jugador ---
+glm::vec3 entityPos = glm::vec3(0.0f, 0.0f, -25.0f);
+float entitySpeed = 2.0f;
+bool entityInitialized = false;
+
+// =========================================================
+// VARIABLES DE LA LINTERNA 
+// =========================================================
+bool flashlightOn = true;
+bool fKeyPressedLastFrame = false;
+float linternaOffsetX = 0.015f;
+float linternaOffsetY = -0.085f;
+float linternaOffsetZ = 0.18f;
+
+// =========================================================
+// LÍMITES DE COLISIÓN (Paredes ajustadas para 4 pasillos)
+// =========================================================
+float roomMinX = -1.8f;
+float roomMaxX = 2.1f;
+float roomMinZ = -18.0f;
+float roomMaxZ = 347.0f;
+float playerHeight = 1.2f;
+// =========================================================
+
+// =========================================================
+// COORDENADAS BASE DE LOS LETREROS (Del Piso)
+// =========================================================
+float baseLightX[5] = { 0.10074f,   0.259016f,  0.0407524f, 0.0422577f, 0.113721f };
+float baseLightZ[5] = { -0.387055f, 17.4112f,   32.5504f,   50.2641f,   65.4025f };
+float roofHeightY = 4.0f;
 
 int main()
 {
@@ -69,43 +108,154 @@ int main()
 
     glEnable(GL_DEPTH_TEST);
 
-    // ... dentro de main(), antes de "Shader ourShader(...)":
     std::cout << "Working directory: " << std::filesystem::current_path() << std::endl;
 
-    // build and compile shaders
-    // -------------------------
     Shader ourShader("shaders/Vertex_Josue.vs", "shaders/Fragment_Josue.fs");
 
-    // load models (ruta relativa, igual que en la escena de Samy)
-    // -------------------------------------------------------------
-    Model ourModel("./model/vr_room/vr_room.obj");
+    Model exitModel("./model/exit/exit.obj");
+    Model entidadModel("./model/exit/entidad.obj");
 
-    camera.MovementSpeed = 10.0f;
+    // Guardamos el tiempo en el que inicia la partida
+    gameStartTime = (float)glfwGetTime();
 
     while (!glfwWindowShouldClose(window))
     {
-        float currentFrame = (float)glfwGetTime();
-        deltaTime = currentFrame - lastFrame;
-        lastFrame = currentFrame;
+        float absoluteTime = (float)glfwGetTime();
+        deltaTime = absoluteTime - lastFrame;
+        lastFrame = absoluteTime;
+
+        // Calculamos cuánto tiempo ha pasado en ESTA vida (se resetea al morir)
+        float currentSceneTime = absoluteTime - gameStartTime;
 
         processInput(window);
 
-        glClearColor(0.05f, 0.05f, 0.08f, 1.0f);
+        // =========================================================
+        // VELOCIDAD DE LA ENTIDAD (LENTA LOS PRIMEROS 7 SEGUNDOS)
+        // =========================================================
+        if (currentSceneTime < 7.0f) {
+            entitySpeed = 2.0f;
+        }
+        else {
+            entitySpeed = 25.0f;
+        }
+
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         ourShader.use();
 
-        glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+        glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 500.0f);
         glm::mat4 view = camera.GetViewMatrix();
         ourShader.setMat4("projection", projection);
         ourShader.setMat4("view", view);
+        ourShader.setVec3("viewPos", camera.Position);
 
-        // render the loaded model
-        glm::mat4 model = glm::mat4(1.0f);
-        model = glm::translate(model, glm::vec3(0.0f, 0.0f, 0.0f));
-        model = glm::scale(model, glm::vec3(1.0f, 1.0f, 1.0f));
-        ourShader.setMat4("model", model);
-        ourModel.Draw(ourShader);
+        ourShader.setVec3("lightDir", glm::vec3(-0.3f, -1.0f, -0.2f));
+        ourShader.setVec3("lightColor", glm::vec3(0.02f, 0.02f, 0.03f));
+        ourShader.setFloat("specularStrength", 0.05f);
+
+        glm::vec3 handOffset = (camera.Right * linternaOffsetX) + (camera.Up * linternaOffsetY) + (camera.Front * linternaOffsetZ);
+        glm::vec3 currentLinternaPos = camera.Position + handOffset;
+
+        ourShader.setVec3("spotLight.position", currentLinternaPos);
+        ourShader.setVec3("spotLight.direction", camera.Front);
+
+        if (flashlightOn) {
+            ourShader.setVec3("spotLight.ambient", glm::vec3(0.0f));
+            ourShader.setVec3("spotLight.diffuse", glm::vec3(2.0f, 2.0f, 1.8f));
+            ourShader.setVec3("spotLight.specular", glm::vec3(1.2f, 1.2f, 1.0f));
+        }
+        else {
+            ourShader.setVec3("spotLight.ambient", glm::vec3(0.0f));
+            ourShader.setVec3("spotLight.diffuse", glm::vec3(0.0f));
+            ourShader.setVec3("spotLight.specular", glm::vec3(0.0f));
+        }
+
+        ourShader.setFloat("spotLight.constant", 1.0f);
+        ourShader.setFloat("spotLight.linear", 0.09f);
+        ourShader.setFloat("spotLight.quadratic", 0.032f);
+        ourShader.setFloat("spotLight.cutOff", glm::cos(glm::radians(10.0f)));
+        ourShader.setFloat("spotLight.outerCutOff", glm::cos(glm::radians(25.0f)));
+
+        int numPasillos = 4;
+        float longitudPasillo = 91.5f;
+        int lightIndex = 0;
+
+        for (int i = 0; i < numPasillos; i++)
+        {
+            glm::mat4 exitMat = glm::mat4(1.0f);
+            exitMat = glm::translate(exitMat, glm::vec3(0.0f, 1.0f * worldScale, i * longitudPasillo));
+            exitMat = glm::scale(exitMat, glm::vec3(worldScale));
+            ourShader.setMat4("model", exitMat);
+            exitModel.Draw(ourShader);
+
+            for (int j = 0; j < 5; j++)
+            {
+                float currentZ = baseLightZ[j] + (i * longitudPasillo);
+                glm::vec3 pos = glm::vec3(baseLightX[j], roofHeightY, currentZ);
+
+                float phase = (float)lightIndex * 2.3f;
+                // Usamos absoluteTime para que las luces no den un salto visual al respawnear
+                float noise = sin(absoluteTime * 12.0f + phase) * cos(absoluteTime * 15.0f + phase * 0.5f);
+                float flicker = (noise > 0.4f) ? 0.15f : 1.0f;
+
+                std::string base = "pointLights[" + std::to_string(lightIndex) + "].";
+                ourShader.setVec3(base + "position", pos);
+
+                ourShader.setVec3(base + "ambient", glm::vec3(0.05f, 0.0f, 0.0f) * flicker);
+                ourShader.setVec3(base + "diffuse", glm::vec3(1.5f, 0.1f, 0.1f) * flicker);
+                ourShader.setVec3(base + "specular", glm::vec3(1.0f, 0.0f, 0.0f) * flicker);
+
+                ourShader.setFloat(base + "constant", 1.0f);
+                ourShader.setFloat(base + "linear", 0.3f);
+                ourShader.setFloat(base + "quadratic", 0.2f);
+
+                lightIndex++;
+            }
+        }
+
+        // =========================================================
+        // MOVIMIENTO DE LA ENTIDAD EN LÍNEA RECTA
+        // =========================================================
+        glm::vec3 direction = glm::vec3(0.0f, 0.0f, camera.Position.z - entityPos.z);
+        float distZ = glm::length(direction);
+
+        if (distZ > 0.05f)
+        {
+            direction = glm::normalize(direction);
+            entityPos += direction * entitySpeed * deltaTime;
+        }
+
+        float angleToPlayer = atan2(direction.x, direction.z);
+
+        glm::mat4 entidadMat = glm::mat4(1.0f);
+        entidadMat = glm::translate(entidadMat, entityPos);
+        entidadMat = glm::rotate(entidadMat, angleToPlayer, glm::vec3(0.0f, 1.0f, 0.0f));
+        entidadMat = glm::scale(entidadMat, glm::vec3(worldScale));
+        ourShader.setMat4("model", entidadMat);
+        entidadModel.Draw(ourShader);
+
+        // =========================================================
+        // COLISIÓN Y RESPAWN (GAME OVER)
+        // =======================================================
+        // Calculamos la distancia solo en el piso (ejes X y Z), ignorando la altura Y de tu cámara
+        float dx = camera.Position.x - entityPos.x;
+        float dz = camera.Position.z - entityPos.z;
+        float captureDistance = sqrt((dx * dx) + (dz * dz));
+
+        float collisionRadius = 2.0f; // Radio más amplio y justo
+
+        if (captureDistance < collisionRadius)
+        {
+            // Reiniciar posición del jugador
+            camera.Position = glm::vec3(0.0f, 1.2f, 1.0f);
+
+            // Reiniciar posición de la entidad
+            entityPos = glm::vec3(0.0f, 0.0f, -25.0f);
+
+            // Reiniciar el temporizador para que la entidad vuelva a ir lenta 7 segundos
+            gameStartTime = (float)glfwGetTime();
+        }
 
         glfwSwapBuffers(window);
         glfwPollEvents();
@@ -115,10 +265,25 @@ int main()
     return 0;
 }
 
+void ClampPlayerToRoom()
+{
+    camera.Position.x = glm::clamp(camera.Position.x, roomMinX, roomMaxX);
+    camera.Position.z = glm::clamp(camera.Position.z, roomMinZ, roomMaxZ);
+    camera.Position.y = playerHeight;
+}
+
 void processInput(GLFWwindow* window)
 {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
+
+    // =========================================================
+    // LÓGICA DE SPRINT CON SHIFT IZQUIERDO
+    // =========================================================
+    if (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS)
+        camera.MovementSpeed = 25.4f;
+    else
+        camera.MovementSpeed = 8.0f; // Velocidad normal lenta
 
     if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
         camera.ProcessKeyboard(FORWARD, deltaTime);
@@ -128,6 +293,25 @@ void processInput(GLFWwindow* window)
         camera.ProcessKeyboard(LEFT, deltaTime);
     if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
         camera.ProcessKeyboard(RIGHT, deltaTime);
+
+    ClampPlayerToRoom();
+
+    bool fPressed = glfwGetKey(window, GLFW_KEY_F) == GLFW_PRESS;
+    if (fPressed && !fKeyPressedLastFrame)
+    {
+        flashlightOn = !flashlightOn;
+    }
+    fKeyPressedLastFrame = fPressed;
+
+    bool pPressed = glfwGetKey(window, GLFW_KEY_P) == GLFW_PRESS;
+    if (pPressed && !pKeyPressedLastFrame)
+    {
+        std::cout << "camera.Position = ("
+            << camera.Position.x << ", "
+            << camera.Position.y << ", "
+            << camera.Position.z << ")" << std::endl;
+    }
+    pKeyPressedLastFrame = pPressed;
 }
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
--- ProyectoBimestral/shaders/Fragment_Josue.fs
+++ ProyectoBimestral/shaders/Fragment_Josue.fs
@@ -2,10 +2,131 @@
 out vec4 FragColor;
 
 in vec2 TexCoords;
+in vec3 Normal;
+in vec3 FragPos;
 
 uniform sampler2D texture_diffuse1;
 
-void main()
-{
-    FragColor = texture(texture_diffuse1, TexCoords);
+uniform vec3 viewPos;
+uniform vec3 lightDir;
+uniform vec3 lightColor;
+uniform float specularStrength;
+
+// =====================================
+// LINTERNA
+// =====================================
+struct SpotLight {
+    vec3 position;
+    vec3 direction;
+    float cutOff;
+    float outerCutOff;
+    float constant;
+    float linear;
+    float quadratic;
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+};
+uniform SpotLight spotLight;
+
+// =====================================
+// LUCES ROJAS DE LOS LETREROS (20 en total)
+// =====================================
+#define NR_POINT_LIGHTS 20
+struct PointLight {
+    vec3 position;
+    float constant;
+    float linear;
+    float quadratic;
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+};
+uniform PointLight pointLights[NR_POINT_LIGHTS];
+
+vec3 CalcSpotLight(SpotLight light, vec3 normal, vec3 fragPos, vec3 viewDir, vec3 texColor) {
+    vec3 lightDir = normalize(light.position - fragPos);
+    float diff = max(dot(normal, lightDir), 0.0);
+    vec3 reflectDir = reflect(-lightDir, normal);
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32.0);
+
+    float distance = length(light.position - fragPos);
+    float attenuation = 1.0 / (light.constant + light.linear * distance + light.quadratic * (distance * distance));
+
+    float theta = dot(lightDir, normalize(-light.direction));
+    float epsilon = light.cutOff - light.outerCutOff;
+    float intensity = clamp((theta - light.outerCutOff) / epsilon, 0.0, 1.0);
+
+    vec3 ambient = light.ambient * texColor;
+    vec3 diffuse = light.diffuse * diff * texColor;
+    vec3 specular = light.specular * spec;
+    
+    return (ambient + diffuse + specular) * attenuation * intensity;
+}
+
+vec3 CalcPointLight(PointLight light, vec3 normal, vec3 fragPos, vec3 viewDir, vec3 texColor) {
+    vec3 lightDir = normalize(light.position - fragPos);
+    float diff = max(dot(normal, lightDir), 0.0);
+    vec3 reflectDir = reflect(-lightDir, normal);
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32.0);
+
+    float distance = length(light.position - fragPos);
+    float attenuation = 1.0 / (light.constant + light.linear * distance + light.quadratic * (distance * distance));
+
+    vec3 ambient = light.ambient * texColor;
+    vec3 diffuse = light.diffuse * diff * texColor;
+    vec3 specular = light.specular * spec;
+    
+    return (ambient + diffuse + specular) * attenuation;
+}
+
+void main() {
+    vec4 texColorRGBA = texture(texture_diffuse1, TexCoords);
+    if (texColorRGBA.a < 0.5) discard;
+    vec3 texColor = texColorRGBA.rgb;
+
+    // --- Ambiente Base ---
+    float ambientStrength = 0.01; 
+    vec3 ambient = ambientStrength * texColor;
+
+    // --- Luz Global ---
+    vec3 norm = normalize(Normal);
+    vec3 lightDirection = normalize(-lightDir);
+    float diff = max(dot(norm, lightDirection), 0.0);
+    vec3 diffuse = diff * texColor * lightColor; 
+
+    vec3 viewDir = normalize(viewPos - FragPos);
+    vec3 halfwayDir = normalize(lightDirection + viewDir);
+    float spec = pow(max(dot(norm, halfwayDir), 0.0), 64.0);
+    vec3 specular = spec * specularStrength * vec3(1.0, 1.0, 1.0);
+
+    // --- Mezcla Principal ---
+    vec3 result = ambient + diffuse + specular;
+
+    // Sumar linterna
+    result += CalcSpotLight(spotLight, norm, FragPos, viewDir, texColor);
+
+    // Sumar todos los puntos de luz rojos
+    for(int i = 0; i < NR_POINT_LIGHTS; i++) {
+        result += CalcPointLight(pointLights[i], norm, FragPos, viewDir, texColor);
+    }
+    
+    // =====================================
+    // LIMITAR ALCANCE DE VISIÓN (NIEBLA NEGRA)
+    // =====================================
+    float fogNear = 7.0;  // Distancia a la que empieza a oscurecerse la visión
+    float fogFar = 25.0;  // Distancia donde ya no ves absolutamente nada (negro total)
+    vec3 fogColor = vec3(0.0, 0.0, 0.0); // Niebla de color negro
+    
+    // Calculamos la distancia real entre tu cámara y el píxel que se está dibujando
+    float dist = length(viewPos - FragPos);
+    
+    // Calculamos qué tan intensa debe ser la oscuridad
+    float fogFactor = (fogFar - dist) / (fogFar - fogNear);
+    fogFactor = clamp(fogFactor, 0.0, 1.0);
+    
+    // Mezclamos tu escena con el negro absoluto según la distancia
+    result = mix(fogColor, result, fogFactor);
+
+    FragColor = vec4(result, texColorRGBA.a);
 }
